ee4c7a7afea4b963e20021492e84d2cb18a65202
Wania Jamal <wania0987shah@gmail.com>
2025-12-01 18:12:03 +0500

    final work

--- middlewares/configMiddleware.js
+++ middlewares/configMiddleware.js
@@ -4,22 +4,35 @@ import categoryRoutes from "../routes/categoryRoutes.js"
 import transactionRoutes from "../routes/transactionRoutes.js";
 import session from "express-session";
 import flash from "connect-flash";
+import MongoStore from "connect-mongo";
 
-function config(app,express, secretKey) {
+function config(app, express, secretKey) {
     app.set("view engine", "ejs");
     app.use(express.static("public"));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    
+
     app.use(session({
         secret: secretKey,
-        resave:false,
-        saveUninitialized: true
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: process.env.DATABASE,
+            collectionName: "sessions",
+            ttl: 60 * 60,
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60,
+            httpOnly: true,
+            secure: false,
+        }
     }));
     app.use(flash());
-    app.use(function(req, res, next){
+    app.use(function (req, res, next) {
         res.locals.success = req.flash("success");
         res.locals.error = req.flash("error");
+        res.locals.formData = req.flash("formData")[0] || {};
+        res.locals.session = req.session;
         next();
     })
 
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -6,10 +6,10 @@ router.get("/", pageController.loginPage);
 router.get("/signup", pageController.signupPage);
 router.get("/dashboard", pageController.dashboardPage);
 router.get("/categories", pageController.categoryPage);
-// router.get("/addCategories", pageController.addCategoryPage);
 router.get("/transaction",pageController.transactionPage);
 router.get("/addTransaction",pageController.addTransactionPage);
 router.get("/profileSetting",pageController.profilePage);
+router.get("/forgetPassword",pageController.forgetPasswordPage);
 
 
 export default router;
